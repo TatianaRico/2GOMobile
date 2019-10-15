e9e28ab068aa6054f02025d9980ccabfad4907fe
Mônica Marques <mo.menezes.marques@gmail.com>
2019-10-14 22:33:48 -0300

    criei lista de confirmados

---
+++ 2GO/View/ListaDeConfirmadosViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListaDeConfirmadosViewController.swift
+//  2GO
+//
+//  Created by Mônica Marques on 14/10/19.
+//  Copyright © 2019 2GO. All rights reserved.
+//
+
+import UIKit
+
+class ListaDeConfirmadosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
