89796b33fda0b6ac786427b9e8fb3cfd02758fc0
Tatiana Rico <tatianarico1702@gmail.com>
2019-10-15 22:13:54 -0300

    criei a tela Home e  fiz a conexão dos elementos da tela na classe

---
+++ 2GO/View/HomeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  HomeCollectionViewCell.swift
+//  2GO
+//
+//  Created by Tatiana Rico on 15/10/19.
+//  Copyright © 2019 2GO. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var eventoImagem: UIImageView!
+    
+    @IBAction func clicouSeguirButton(_ sender: UIButton) {
+    }
+}
---
+++ 2GO/View/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomeViewController.swift
+//  2GO
+//
+//  Created by Tatiana Rico on 15/10/19.
+//  Copyright © 2019 2GO. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var searchView: UIView!
+    @IBOutlet weak var searchUiSearch: UISearchBar!
+    
+    @IBOutlet weak var eventoImagem: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
