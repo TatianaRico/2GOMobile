0e2c0a0eb1125442f2d9e67a4783d1482f1b23a3
Tatiana Rico <tatianarico1702@gmail.com>
2019-10-17 10:08:13 -0300

    coloquei o map na view e criei a class map

---
+++ 2GO/View/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  2GO
+//
+//  Created by Tatiana Rico on 17/10/19.
+//  Copyright © 2019 2GO. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
