518c959ec0ec69032d98b0047129af240e570519
Tatiana Rico <tatianarico1702@gmail.com>
2019-11-06 22:13:17 -0300

    criei a pasta model e provider...coloquei o json categoria

---
+++ 2GO/Model/Categoria.swift
@@ -0,0 +1,45 @@
+//
+//  Categoria.swift
+//  2GO
+//
+//  Created by Tatiana Rico on 06/11/19.
+//  Copyright © 2019 2GO. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Categoria
+struct Categoria: Codable {
+    let locale: String
+    let pagination: Pagination
+    let categories: [Category]
+}
+
+// MARK: - Category
+struct Category: Codable {
+    let resourceURI: String
+    let id, name, nameLocalized, shortName: String
+    let shortNameLocalized: String
+
+    enum CodingKeys: String, CodingKey {
+        case resourceURI = "resource_uri"
+        case id, name
+        case nameLocalized = "name_localized"
+        case shortName = "short_name"
+        case shortNameLocalized = "short_name_localized"
+    }
+}
+
+// MARK: - Pagination
+struct Pagination: Codable {
+    let objectCount, pageNumber, pageSize, pageCount: Int
+    let hasMoreItems: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case objectCount = "object_count"
+        case pageNumber = "page_number"
+        case pageSize = "page_size"
+        case pageCount = "page_count"
+        case hasMoreItems = "has_more_items"
+    }
+}
