89b17be748768af8aec78205ef6e534f809bb2c6
Mônica Marques <mo.menezes.marques@gmail.com>
2019-12-08 18:36:15 -0300

    Tentando arrumar a colectionView, mas a imagem nao esta carregando

--- 2GO/View/Cell/HomeCollectionViewCell.swift
+++ 2GO/View/Cell/HomeCollectionViewCell.swift
@@ -11,16 +11,22 @@ import UIKit
 class HomeCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var titleCategoryLabel: UILabel!
+    @IBOutlet weak var eventoImage: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.layer.borderWidth = 2
-        self.layer.borderColor = UIColor.init(red: CGFloat(Int.random(in: 0...50))/255, green: CGFloat(Int.random(in: 0...50))/255, blue: CGFloat(Int.random(in: 240...255))/255, alpha: 1).cgColor
-        self.backgroundColor = .lightGray
-        self.layer.cornerRadius = 4.0
+          var localSp: Business?
+        
+        
+        override func awakeFromNib() {
+            super.awakeFromNib()
+    //        self.layer.borderWidth = 2
+    //        self.layer.borderColor = UIColor.init(red: CGFloat(Int.random(in: 0...50))/255, green: CGFloat(Int.random(in: 0...50))/255, blue: CGFloat(Int.random(in: 240...255))/255, alpha: 1).cgColor
+    //        self.backgroundColor = .lightGray
+    //        self.layer.cornerRadius = 4.0
+        }
+
+        func setup(bussiness: Business) {
+            titleCategoryLabel.text = bussiness.name
+            eventoImage?.sd_setImage(with: URL(string: localSp?.imageURL ?? ""))
+        }
     }
 
-    func setup(bussiness: Business) {
-        titleCategoryLabel.text = bussiness.name
-    }
-}
